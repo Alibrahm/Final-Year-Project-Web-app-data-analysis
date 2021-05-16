6b654336898913cf14c27d08023fcbfecd3f315c
ALI IBRAHIM <alibra71@gmail.com>
2021-05-16 23:28:22 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 mkdir -p ~/.streamlit/
-
-echo "[theme]
-primaryColor = ‘#D33682’
-backgroundColor = ‘#002B36’
-secondaryBackgroundColor = ‘#586E75’
-textColor= ‘#FAFAFA’
-font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
